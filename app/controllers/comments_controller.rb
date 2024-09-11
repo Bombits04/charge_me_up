ab99411ddea4508e79d0b979a67d30bcc932ece7
Bombits04 <bombits.03+github@gmail.com>
2024-09-12 05:01:04 +0800

    added commenting and creating of charging spots

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,22 @@
 class CommentsController < ApplicationController
+  before_action :set_chg_spot
+
   def create
-    @chg_spot = ChgSpot.find(params[:chg_spot_id])
+    @comment = @chg_spot.comments.build(comment_params)
+    @comment.user_id = current_user.id
 
-    @comment = @chg_spot.comments.create(comment_params)
-    @comment.chg_spot_id = @chg_spot.id
-    @comment.user_id = 1
-
-    @comment.save
-
-    redirect_to chg_spot_path(@chg_spot)
+    if @comment.save
+      redirect_to chg_spot_path(@chg_spot), notice: "Comment was successfully created."
+    else
+      redirect_to chg_spot_path(@chg_spot), alert: "Failed to create comment."
+    end
   end
 
   private
+
+  def set_chg_spot
+    @chg_spot = ChgSpot.find(params[:chg_spot_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:comment)
